90529cc62e64c330ffa41d167f9aa87433b5ea89
Matthew-Dombrady <matthew.dombrady@gmail.com>
2019-03-28 16:51:21 -0400

    Added a folder for test level

---
+++ TestLevel/Tower Defense Test/Assets/ArenaController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaController : MonoBehaviour {
+
+    public Transform[] enemies;
+
+    private float nextWaveCount = 15f;
+    private float waveCountdown = 5f;
+    private float timeBetweenEnemies = 2f;
+    private int waveNum = 0;
+
+    public Transform startPoint;
+
+
+    public GameObject gameOverScreen;
+
+	void Update () {
+
+        if (waveCountdown <= 0f) {
+            StartCoroutine(createWave());
+            waveCountdown = nextWaveCount;
+        }
+
+        waveCountdown = waveCountdown - Time.deltaTime;
+
+
+        
+
+	}
+
+
+    IEnumerator createWave() {
+
+        for (int i = 0; i < 5; i++) {
+            spawnEnemy();
+
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+
+
+        waveNum++;
+    }
+
+
+    private void spawnEnemy() {
+        Instantiate(enemies[waveNum % 2], startPoint.position, startPoint.rotation);
+    }
+
+    public void gameOver() {
+        gameOverScreen.gameObject.SetActive(true);
+    }
+}
---
+++ TestLevel/Tower Defense Test/Assets/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+    private Transform currentCheckpoint;
+    private int checkpointIndex = 0;
+
+    public ArenaController arena;
+
+	void Start () {
+        currentCheckpoint = Checkpoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 direction = currentCheckpoint.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime , Space.World);
+
+        if (Vector3.Distance(transform.position, currentCheckpoint.position) <= 0.2f) {
+            incrementCheckpoint();
+        }
+	}
+
+    private void incrementCheckpoint() {
+
+        if (checkpointIndex >= Checkpoints.points.Length - 1) {
+
+            Destroy(gameObject);
+
+            //ArenaController.gameOverScreen.SetActive(true);
+
+            arena.gameOver();
+
+            return;
+        }
+
+        checkpointIndex++;
+        currentCheckpoint = Checkpoints.points[checkpointIndex];
+    }
+}
